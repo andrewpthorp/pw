b073721e085179c4855328f92d48b1b7a824bdc5
Andrew Thorp <andrewpthorp@gmail.com>
2015-04-15 20:35:11 -0500

    no mocks

--- mix.exs
+++ mix.exs
@@ -9,25 +9,12 @@ defmodule PW.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :porcelain]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    [{:porcelain, "~> 2.0.0"},
-     {:mock, "~> 0.1.0", only: :test}]
+    [{:porcelain, "~> 2.0.0"}]
   end
 
   defp escript_config do
